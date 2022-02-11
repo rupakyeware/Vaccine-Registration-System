1efc9ea7a48cc869f4a9de9567b471295feaf270
rupakyeware <rupakyeware@gmail.com>
2022-02-11 09:28:58 +0530

    Final Commit

--- system.c
+++ system.c
@@ -1,138 +1,181 @@
-// User name, dob, age, addhar , Gender, Time slot //
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
-struct Student
+struct Vaccine
 {
-    int rollnumber;
+    int AadharId;
     char name[100];
     char phone[100];
-    float percentage;
-    struct Student *next;
+    int TimeSlot;
+    struct Vaccine *next;
 
 } * head;
-void insert(int rollnumber, char *name, char *phone, float percentage)
+//---------------------------------------------------------------------------------------------------------------//
+void insert(int AadharId, char *name, char *phone, float TimeSlot)
 {
 
-    struct Student *student = (struct Student *)malloc(sizeof(struct Student));
-    student->rollnumber = rollnumber;
+    struct Vaccine *student = (struct Vaccine *)malloc(sizeof(struct Vaccine));
+    student->AadharId = AadharId;
     strcpy(student->name, name);
     strcpy(student->phone, phone);
-    student->percentage = percentage;
+    student->TimeSlot = TimeSlot;
     student->next = NULL;
 
     if (head == NULL)
     {
-        // if head is NULL
-        // set student as the new head
+
         head = student;
     }
     else
     {
-        // if list is not empty
-        // insert student in beginning of head
         student->next = head;
         head = student;
     }
 }
-void search(int rollnumber)
+//------------------------------------------------------------------------------------------------------------------//
+// searching registration data (by aadhar id)
+void search(int AadharId)
 {
-    struct Student *temp = head;
+    struct Vaccine *temp = head;
+    printf("\n---------------------------------------------------------------------------\n");
     while (temp != NULL)
     {
-        if (temp->rollnumber == rollnumber)
+        if (temp->AadharId == AadharId)
         {
-            printf("Roll Number: %d\n", temp->rollnumber);
-            printf("Name: %s\n", temp->name);
-            printf("Phone: %s\n", temp->phone);
-            printf("Percentage: %0.4f\n", temp->percentage);
+            printf("\nName: %s\n", temp->name);
+            printf("\nAadhar id: %d\n", temp->AadharId);
+            printf("\nPhone: %s\n", temp->phone);
+            if (temp->TimeSlot == 1)
+            {
+                printf("\nYour Time slot is :\n\n1->10:00 to 11:00AM", temp->TimeSlot);
+                printf("\n---------------------------------------------------------------------------\n");
+            }
+            else if (temp->TimeSlot == 2)
+            {
+                printf("\nYour Time slot is :\n\n2->11:00 to 12:00PM", temp->TimeSlot);
+                printf("\n---------------------------------------------------------------------------\n");
+            }
+            else
+            {
+                printf("\nYour Time slot is :\n\n2->12:00 to 1:00PM", temp->TimeSlot);
+                printf("\n---------------------------------------------------------------------------\n");
+            }
             return;
         }
         temp = temp->next;
     }
-    printf("Student with roll number %d is not found !!!\n", rollnumber);
+    printf("Vaccine with vaccine id %d is not found !!!\n", AadharId);
 }
-void update(int rollnumber)
+//-----------------------------------------------------------------------------------------------------------------------//
+// updating member details
+void update(int AadharId)
 {
 
-    struct Student *temp = head;
+    struct Vaccine *temp = head;
     while (temp != NULL)
     {
 
-        if (temp->rollnumber == rollnumber)
+        if (temp->AadharId == AadharId)
         {
-            printf("Record with roll number %d Found !!!\n", rollnumber);
-            printf("Enter new name: ");
+            printf("\nRecord with vaccine id %d Found !!!\n", AadharId);
+            printf("\nEnter new name: ");
             scanf("%s", temp->name);
-            printf("Enter new phone number: ");
+            printf("\nEnter new phone number: ");
             scanf("%s", temp->phone);
-            printf("Enter new percentage: ");
-            scanf("%f", &temp->percentage);
-            printf("Updation Successful!!!\n");
+            printf("\nEnter new TimeSlot: ");
+            printf("\n1-> 10:00 to 11:00AM\n\n2-> 11:00 to 12:00PM\n\n3-> 12:00 to 1:00PM\n");
+            printf("\nSelect Time slot :");
+            scanf("%d", &temp->TimeSlot);
+            printf("\nUpdation Successful!!!\n");
+            printf("\n-------------------------------------------------------------------------\n");
             return;
         }
         temp = temp->next;
     }
-    printf("Student with roll number %d is not found !!!\n", rollnumber);
+    printf("No data found for Aadhar id %d  !!!\n", AadharId);
 }
-void Delete(int rollnumber)
+//---------------------------------------------------------------------------------------------------------------------//
+// cancel- registration
+void Delete(int AadharId)
 {
-    struct Student *temp1 = head;
-    struct Student *temp2 = head;
+    struct Vaccine *temp1 = head;
+    struct Vaccine *temp2 = head;
     while (temp1 != NULL)
     {
 
-        if (temp1->rollnumber == rollnumber)
+        if (temp1->AadharId == AadharId)
         {
 
-            printf("Record with roll number %d Found !!!\n", rollnumber);
+            printf("\n\nRecord with Aadhar id %d Found !!!\n\n", AadharId);
 
             if (temp1 == temp2)
             {
-                // this condition will run if
-                // the record that we need to delete is the first node
-                // of the linked list
+
                 head = head->next;
                 free(temp1);
             }
             else
             {
-                // temp1 is the node we need to delete
-                // temp2 is the node previous to temp1
+
                 temp2->next = temp1->next;
                 free(temp1);
             }
 
-            printf("Record Successfully Deleted !!!\n");
+            printf("\nRecord Successfully Deleted !!!\n\n");
+            printf("\n-------------------------------------------------------------------------\n");
             return;
         }
         temp2 = temp1;
         temp1 = temp1->next;
     }
-    printf("Student with roll number %d is not found !!!\n", rollnumber);
+    printf("\n\nNo data found for Aadhar id %d  !!!\n\n", AadharId);
 }
+//----------------------------------------------------------------------------------------------------------------------//
+// display-member data
 void display()
 {
-    struct Student *temp = head;
+    struct Vaccine *temp = head;
+    printf("\n---------------------------------------------------------------------------\n");
     while (temp != NULL)
     {
 
-        printf("Roll Number: %d\n", temp->rollnumber);
-        printf("Name: %s\n", temp->name);
-        printf("Phone: %s\n", temp->phone);
-        printf("Percentage: %0.4f\n\n", temp->percentage);
+        printf("\nAadhar id: %d\n", temp->AadharId);
+        printf("\nName: %s\n", temp->name);
+        printf("\nPhone: %s\n", temp->phone);
+        if (temp->TimeSlot == 1)
+        {
+            printf("\nYour Time slot is :\n\n1.10:00 to 11:00AM\n", temp->TimeSlot);
+            printf("\n***********\n");
+        }
+        else if (temp->TimeSlot == 2)
+        {
+            printf("\nYour Time slot is :\n\n2.11:00 to 12:00PM\n", temp->TimeSlot);
+            printf("\n***********\n");
+        }
+        else
+        {
+            printf("\nYour Time slot is :\n\n2.12:00 to 1:00PM\n", temp->TimeSlot);
+            printf("\n***********\n");
+        }
         temp = temp->next;
     }
+    printf("\n---------------------------------------------------------------------------\n");
 }
+//--------------------------------------------------------------------------------------------------------------------------//
+// driver program
 int main()
 {
     head = NULL;
     int choice;
     char name[100];
     char phone[100];
-    int rollnumber;
-    float percentage;
-    printf("1 to insert student details\n2 to search for student details\n3 to delete student details\n4 to update student details\n5 to display all student details");
+    int AadharId;
+    float TimeSlot;
+    printf("-------------------------------------------------------------------------\n");
+    printf("\t\t\tVACCINE REGISTRATION\t\t\t\n");
+    printf("-------------------------------------------------------------------------\n");
+    printf("\n1  Register for vaccine\n2  Check registration details\n3  Cancel registration\n4  Update details\n5  Display all\n6 Exit");
+    printf("\n-------------------------------------------------------------------------\n");
     do
     {
         printf("\nEnter Choice: ");
@@ -140,34 +183,59 @@ int main()
         switch (choice)
         {
         case 1:
-            printf("Enter roll number: ");
-            scanf("%d", &rollnumber);
-            printf("Enter name: ");
+
+            printf("\n\n\t\t\t*** REGISTRATION ***\t\t\t\n\n");
+
+            printf("\nEnter aadhar id:");
+            scanf("%d", &AadharId);
+            printf("\nEnter name: ");
             scanf("%s", name);
-            printf("Enter phone number: ");
+            printf("\nEnter phone number: ");
             scanf("%s", phone);
-            printf("Enter percentage: ");
-            scanf("%f", &percentage);
-            insert(rollnumber, name, phone, percentage);
+            printf("\n1-> 10:00 to 11:00AM\n\n2-> 11:00 to 12:00PM\n\n3-> 12:00 to 1:00PM\n");
+            printf("\nSelect Time slot :");
+            scanf("%d", &TimeSlot);
+
+            printf("\nSUCCESFULLY REGISTERED FOR VACCINATION!!\n");
+            int vacId = rand();
+            printf("\nYOUR SECRET CODE/VACCINE ID IS %d \n", rand());
+            printf("\n-------------------------------------------------------------------------\n");
+            insert(AadharId, name, phone, TimeSlot);
             break;
         case 2:
-            printf("Enter roll number to search: ");
-            scanf("%d", &rollnumber);
-            search(rollnumber);
+
+            printf("\n\n\t\t\t**REGISTRATION DETAILS***\t\t\t\n\n");
+
+            printf("Enter Aadhar id to search: ");
+            scanf("%d", &vacId);
+            search(AadharId);
             break;
         case 3:
-            printf("Enter roll number to delete: ");
-            scanf("%d", &rollnumber);
-            Delete(rollnumber);
+
+            printf("\n\n\t\t\t***CANCEL REGISTRATION***\t\t\t\n\n");
+
+            printf("Enter Aadhar id to delete: ");
+            scanf("%d", &AadharId);
+            Delete(AadharId);
             break;
         case 4:
-            printf("Enter roll number to update: ");
-            scanf("%d", &rollnumber);
-            update(rollnumber);
+
+            printf("\n\n\t\t\t**UPDATE REGISTRATION DETAILS*\t\t\t\n\n");
+
+            printf("Enter Aadhar id to update: ");
+            scanf("%d", &AadharId);
+            update(AadharId);
             break;
         case 5:
+
+            printf("\n\n\t\t\t**REGISTRATION DATA*\t\t\t\n\n");
+
             display();
             break;
+
+        case 6:
+            printf("\nTHANK YOU FOR REGISTERING.STAY HOME!! STAY SAFE!!\n\n");
+            exit(0);
         }
 
     } while (choice != 0);
